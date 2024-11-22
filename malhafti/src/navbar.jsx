8e9e8429737e8a7030039c185c4f88753c22ee16
SEHUN <khaoulabhaou@gmail.com>
2024-11-22 15:42:11 +0100

    added navbar function to navbar.jsx

---
+++ malhafti/src/navbar.jsx
@@ -0,0 +1,33 @@
+export default function Navbar(){
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+            <a href="" className="navbar-brand">ملحفتي</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+                <ul className="navbar-nav">
+                    <li className="nav-item">
+                        <a href="" className="nav-link">الصفحة الرئيسية</a>
+                    </li>
+                    <li className="nav-item">
+                        <a href="" className="nav-link">الملاحف</a>
+                    </li>
+                    <li className="nav-item">   
+                        <a href="" className="nav-link">الرمبايات</a>
+                    </li> 
+                    <li className="nav-item">
+                        <a href="" className="nav-link">البوديات</a>
+                    </li>
+                    <li className="nav-item">
+                        <a href="" className="nav-link">النعايل</a>
+                    </li>
+                    <li className="nav-item">
+                        <a href="" className="nav-link">الصيكان</a>
+                    </li>
+                    <li className="nav-item">
+                        <a href="" className="nav-link">الديار</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+}
